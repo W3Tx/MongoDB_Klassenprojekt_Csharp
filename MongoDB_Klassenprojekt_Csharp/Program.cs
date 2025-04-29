75995f3527e5ba3c43d4fee75632119eaf43baf2
W3Tx <foto.martindkawan@hotmail.com>
2025-04-29 07:44:51 +0200

    Projektdateien hinzufügen.

---
+++ MongoDB_Klassenprojekt_Csharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB;
+using MongoDB.Bson;
+using System;
+using System.Linq;
+
+internal class Program
+{
+    // --- MongoDB Einstellungen --- 
+
+
+    // -------------------------------
+
+    static void Main(string[] args)
+    {
+        // --- Dein Program einfach hier reinschreiben ---
+
+
+
+        // -----------------------------------------------
+    }
+}
