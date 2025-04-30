44d5970b6d7edfb1b344103f6ad599e955525247
W3Tx <foto.martindkawan@hotmail.com>
2025-04-30 10:01:35 +0200

    Update

--- MongoDB_Klassenprojekt_Csharp/Program.cs
+++ MongoDB_Klassenprojekt_Csharp/Program.cs
@@ -1,21 +1,64 @@
 ﻿using MongoDB;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
 using System;
 using System.Linq;
 
-internal class Program
+internal class Program 
 {
-    // --- MongoDB Einstellungen --- 
-
-
-    // -------------------------------
-
+    static IMongoClient client;
+    static IMongoDatabase database;
     static void Main(string[] args)
     {
         // --- Dein Program einfach hier reinschreiben ---
 
+        client = new MongoClient("mongodb://localhost:27017");
+        Console.WriteLine("Verbunden mit MongoDB-Server");
 
+        ListDatabases();
+        SelectOrCreateDatabase();
+        ListCollections();
 
         // -----------------------------------------------
     }
+
+
+    static void ListDatabases()
+    {
+        var databases = client.ListDatabaseNames().ToList();
+        Console.WriteLine("\nVerfügbare Datenbanken: ");
+        foreach (var db in databases)
+        {
+            Console.WriteLine($"- {db}");
+        }
+    }
+
+
+    static void SelectOrCreateDatabase()
+    {
+        Console.WriteLine("\nGeben Sie den Namen der Datenbank ein");
+        var dbName = Console.ReadLine().Trim();
+        database = client.GetDatabase(dbName);
+        Console.WriteLine($"Datanbank '{dbName}' wurde ausgewählt oder wird erstellt");
+    }
+
+
+    static void ListCollections()
+    {
+        if (database == null)
+        {
+            Console.WriteLine("Bitte eine Datenbank auswählen soll");
+            return;
+        }
+
+        var collections = database.ListCollectionNames().ToList();
+        Console.WriteLine("\nVerfügbare Sammlungen: ");
+        foreach (var col in collections)
+        {
+            Console.WriteLine($"- {col}");
+        } 
+    }
+
+
 }
