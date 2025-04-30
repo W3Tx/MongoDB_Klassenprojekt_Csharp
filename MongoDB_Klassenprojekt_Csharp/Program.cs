e9934fb81aa74a02b53636eb786e99554efb8739
W3Tx <foto.martindkawan@hotmail.com>
2025-04-30 11:11:35 +0200

    Update

--- MongoDB_Klassenprojekt_Csharp/Program.cs
+++ MongoDB_Klassenprojekt_Csharp/Program.cs
@@ -9,6 +9,8 @@ internal class Program
 {
     static IMongoClient client;
     static IMongoDatabase database;
+    static IMongoCollection<BsonDocument> collection;
+
     static void Main(string[] args)
     {
         // --- Dein Program einfach hier reinschreiben ---
@@ -19,6 +21,9 @@ internal class Program
         ListDatabases();
         SelectOrCreateDatabase();
         ListCollections();
+        SelectOrCreateCollection();
+        InsertDocument();
+        DisplayDocument();
 
         // -----------------------------------------------
     }
@@ -60,5 +65,79 @@ internal class Program
         } 
     }
 
+    static void SelectOrCreateCollection()
+    {
+        if (database == null)
+        {
+            Console.WriteLine("Bitte eine Datenbank auswählen soll");
+            return;
+        }
+
+        Console.WriteLine("\nGeben Sie den Namen der Collection ein");
+        var colName = Console.ReadLine().Trim();
+        collection = database.GetCollection<BsonDocument>(colName);
+        Console.WriteLine($"Collection '{colName}' ausgewählt. (oder bei Bedarf erstellt)");
+    }
+
+    static void InsertDocument()
+    {
+        if (collection == null)
+        {
+            Console.WriteLine("Bitte eine Collection auswählen");
+            return;
+        }
+
+        Console.WriteLine("\nGeben Sie den Name ein: ");
+        var name = Console.ReadLine().Trim();
+
+        Console.WriteLine("\nGeben Sie das Alter ein: ");
+        var alterInput = Console.ReadLine().Trim();
+        int alter = int.TryParse(alterInput, out var parsedAlter) ? parsedAlter : 0;
+
+        var dokument = new BsonDocument
+        {
+            { "Name", name },
+            { "Alter", alter }
+        };
+
+        collection.InsertOne(dokument);
+        Console.WriteLine("Dokument erfolgreich eingefügt");    
+    }
+
+    static void DisplayDocument()
+    {
+        if (collection == null)
+        {
+            Console.WriteLine("Bitte eine Collection auswählen");
+            return;
+        }
+
+        var document = collection.Find(new BsonDocument()).ToList();
+        Console.WriteLine("\nAlle Dokumente");
+        foreach (var doc in document)
+        {
+            Console.WriteLine(doc.ToString());
+        }
+    }
+
+    static void DeleteSingleDocument()
+    {
+        if (collection == null)
+        {
+            Console.WriteLine("Bitte zuerst eine Collection auswählen");
+            return;
+        }
+
+        Console.Write("\ngebe Sie den Namen des zu löschenden Dokuments ein: ");
+        var name = Console.ReadLine().Trim();
+
+        var filter = Builders<BsonDocument>.Filter.Eq("Name", name);
+        var result = collection.DeleteOne(filter);
+
+        if (result.DeletedCount > 0)
+            Console.WriteLine($"Dokument '{name}' erfolgreich gelöscht");
+        else
+            Console.WriteLine($"Dokument '{name}' nicht gefunden");
+    }
 
 }
